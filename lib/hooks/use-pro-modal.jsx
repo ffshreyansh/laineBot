8a51c424c8808df52aa9787fbc9952a84afdfb9a
Shreyansh Kumar <shreyanshkumar058@gmail.com>
2024-02-12 09:45:26 +0530

    bug fix 3

--- lib/hooks/use-pro-modal.jsx
+++ lib/hooks/use-pro-modal.jsx
@@ -1,7 +1,7 @@
 import { create } from 'zustand';
 
 export const useProModal = create((set) => ({
-  isOpen: true,
+  isOpen: false,
   onOpen: () => set({ isOpen: true }),
   onClose: () => set({ isOpen: false }),
 }));
