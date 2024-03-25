938fb0db4d4883e35ed6819e46b70da59044f05a
Siddhartha Singh <84330754+siddhartha-up80@users.noreply.github.com>
2024-03-25 23:08:39 +0530

    done basic blog slug page

--- app/(Blog)/blog/[slug]/page.jsx
+++ app/(Blog)/blog/[slug]/page.jsx
@@ -1,11 +1,20 @@
 import BlogContainer from "@/components/blog-container";
 import { client } from "@/sanity/lib/client";
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 async function getData(slug) {
   const post = await client.fetch(
     `
-    *[_type == "post" && slug.current == "${slug}"][0]{title, "name": author->name}
+    *[_type == "post" && slug.current == "${slug}"][0]{
+      title,
+      "name": author->name,
+      "slug": slug.current,
+      description,
+      mainImage,
+      author->{"name": name}
+    }
   `,
     { slug }
   );
@@ -18,15 +27,48 @@ const BlogPost = async ({ params }) => {
   // console.log(params);
   const data = await getData(params.slug);
 
-  // console.log(data);
+  console.log(data);
 
   return (
     <div className="max-w-7xl mx-auto my-20">
       <div>
         <h1 className="text-7xl font-bold">Blog</h1>
       </div>
-      {data?.title}
-      {data?.name}
+      <div>
+        <div className="mx-auto mt-10">
+          <div className="flex flex-col max-w-7xl mx-auto overflow-hidden rounded">
+            <Image
+              src="https://source.unsplash.com/random/480x360"
+              height={700}
+              width={1000}
+              alt=""
+              className="w-full h-[70vh] object-cover object-center"
+            />
+            <div className="p-6 pb-12 m-4 mx-auto -mt-16 space-y-6 lg:max-w-5xl sm:px-10 lg:rounded-md shadow-lg bg-white dark:bg-black">
+              <div className="space-y-2">
+                <span className="inline-block text-2xl font-semibold sm:text-3xl">
+                  {data.title}
+                </span>
+                <p className="text-xs ">
+                  By{" "}
+                  <span className="text-xs hover:underline">{data.name}</span>
+                </p>
+              </div>
+              <div className="">
+                <p>
+                  {data?.description} Lorem ipsum dolor sit amet consectetur
+                  adipisicing elit. Esse libero, beatae reiciendis magnam
+                  eveniet eligendi voluptatum eum exercitationem repudiandae
+                  voluptatibus doloribus. Possimus incidunt unde temporibus modi
+                  eaque nemo sapiente. Quia consequuntur voluptas modi incidunt
+                  esse? Nesciunt voluptatum esse molestias, ea quisquam quidem
+                  quaerat vel corporis aut earum ex doloribus placeat?
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- components/blog-container.jsx
+++ components/blog-container.jsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 const BlogContainer = ({ data }) => {
   return (
@@ -18,35 +19,36 @@ const BlogContainer = ({ data }) => {
         <span className="text-5xl font-extrabold">More Stories</span>
         <div>
           <div className="mx-auto space-y-8">
-            <div className="grid grid-cols-1 gap-x-4 gap-y-8 md:grid-cols-2 ">
+            <div className="grid grid-cols-1 gap-x-6 gap-y-8 md:grid-cols-2 ">
               {data.map((post) => (
-                <article key={post._id} className="flex flex-col">
-                  <div aria-label={post.title}>
-                    <Image
-                      width={500}
-                      height={500}
-                      alt=""
-                      className="object-cover w-full h-52 hover:shadow-xl hover:shadow-gray-400 rounded-md shadow-md duration-300"
-                      src={
-                        post?.mainImage?.asset?.url ||
-                        "https://source.unsplash.com/random/1080×1920/?girl,woman=2"
-                      }
-                    />
-                  </div>
-                  <div className="flex flex-col flex-1 py-6">
-                    <div></div>
-                    <div className="text-xs tracki uppercase hover:underline text-rose-600">
-                      {post.author.name}
+                <Link href={`/blog/${post.slug}`}>
+                  <article key={post._id} className="flex flex-col">
+                    <div aria-label={post.title}>
+                      <Image
+                        width={500}
+                        height={500}
+                        alt=""
+                        className="object-cover w-[90%] h-52 hover:shadow-xl hover:shadow-gray-400 rounded-md shadow-md duration-300"
+                        src={
+                          post?.mainImage?.asset?.url ||
+                          "https://source.unsplash.com/random/1080×1920/?girl,woman=2"
+                        }
+                      />
                     </div>
-                    <h3 className="flex-1 py-2 text-lg font-semibold">
-                      {post.title}
-                    </h3>
-                    <p>{post.description}</p>
-                    <div className="flex flex-wrap justify-between pt-3 space-x-2 text-xs">
-                      <span>{post._createdAt}</span>{" "}
+                    <div className="flex flex-col flex-1 py-6">
+                      <div className="text-xs tracki uppercase hover:underline text-rose-600">
+                        {post.author.name}
+                      </div>
+                      <h3 className="flex-1 py-2 text-lg font-semibold">
+                        {post.title}
+                      </h3>
+                      <p>{post.description}</p>
+                      <div className="flex flex-wrap justify-between pt-3 space-x-2 text-xs">
+                        <span>{post._createdAt}</span>{" "}
+                      </div>
                     </div>
-                  </div>
-                </article>
+                  </article>
+                </Link>
               ))}
             </div>
           </div>
