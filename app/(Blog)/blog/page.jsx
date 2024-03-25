b1acd4108c315c8e2b6c0a5d62ddb583d90c4142
Siddhartha Singh <84330754+siddhartha-up80@users.noreply.github.com>
2024-03-25 16:54:43 +0530

    blog page basic done

---
+++ app/(Blog)/blog/page.jsx
@@ -0,0 +1,38 @@
+import BlogContainer from "@/components/blog-container";
+import { client } from "@/sanity/lib/client";
+import React from "react";
+
+async function getData() {
+  const post = await client.fetch(
+    `
+    *[_type == "post"] {
+      title,
+      "name": author->name,
+      description,
+      mainImage,
+      author->{"name": name}
+    }
+    `
+  );
+  return post;
+}
+
+export const dynamic = "force-dynamic";
+
+const Blog = async () => {
+  // console.log(params);
+  const data = await getData();
+
+  //   console.log(data);
+
+  return (
+    <div className="max-w-6xl mx-auto my-20 space-y-10">
+      <div>
+        <h1 className="text-7xl font-extrabold">Blog</h1>
+      </div>
+      <BlogContainer data={data} />
+    </div>
+  );
+};
+
+export default Blog;
---
+++ components/blog-container.jsx
@@ -0,0 +1,59 @@
+"use client";
+
+import React from "react";
+import Image from "next/image";
+
+const BlogContainer = ({ data }) => {
+  return (
+    <div className="space-y-10">
+      <div>
+        <Image
+          src={"https://source.unsplash.com/random/1080×1920/?girl,woman=1"}
+          height={1000}
+          width={1000}
+          className="w-full aspect-video object-cover object-center hover:shadow-xl hover:shadow-gray-400 rounded-md shadow-md duration-300"
+        />
+      </div>
+      <div className="space-y-10">
+        <span className="text-5xl font-extrabold">More Stories</span>
+        <div>
+          <div className="mx-auto space-y-8">
+            <div className="grid grid-cols-1 gap-x-4 gap-y-8 md:grid-cols-2 ">
+              {data.map((post) => (
+                <article key={post._id} className="flex flex-col">
+                  <div aria-label={post.title}>
+                    <Image
+                      width={500}
+                      height={500}
+                      alt=""
+                      className="object-cover w-full h-52 hover:shadow-xl hover:shadow-gray-400 rounded-md shadow-md duration-300"
+                      src={
+                        post?.mainImage?.asset?.url ||
+                        "https://source.unsplash.com/random/1080×1920/?girl,woman=2"
+                      }
+                    />
+                  </div>
+                  <div className="flex flex-col flex-1 py-6">
+                    <div></div>
+                    <div className="text-xs tracki uppercase hover:underline text-rose-600">
+                      {post.author.name}
+                    </div>
+                    <h3 className="flex-1 py-2 text-lg font-semibold">
+                      {post.title}
+                    </h3>
+                    <p>{post.description}</p>
+                    <div className="flex flex-wrap justify-between pt-3 space-x-2 text-xs">
+                      <span>{post._createdAt}</span>{" "}
+                    </div>
+                  </div>
+                </article>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BlogContainer;
